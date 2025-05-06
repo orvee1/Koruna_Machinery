8cbe32a107ab1a1a1d352ddb33489135cefc3243
orvee1 <imrulibtehaz8@gmail.com>
2025-05-07 00:25:11 +0600

    Updated stock migrations

--- database/migrations/2025_03_26_134625_create_stocks_table.php
+++ database/migrations/2025_03_26_134625_create_stocks_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('branch_id')->constrained()->onDelete('cascade');
-            // $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('product_name');            
             $table->string('supplier_name');
             $table->decimal('buying_price', 10, 2);
